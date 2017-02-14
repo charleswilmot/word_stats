b44ee31fa885c0205ac36fac22f4eb6bbaba619a
Charles Wilmot <cwilmot@localhost.localdomain>
2017-02-14 19:18:22 +0100

    cleanup

--- src/wstats.py
+++ src/wstats.py
@@ -33,3 +33,28 @@ class FrameIterator:
 
     def next(self) :
         pass
+
+
+def entropy(freq_tab):
+    """
+    returns the entropy
+    """
+    pass
+
+def get_letter_entropy(txt, n, letters_set):
+    return entropy(get_letter_freq(txt, n, letters_set))
+
+def get_word_entropy(txt, word_end_set):
+    return entropy(get_word_freq(txt, word_end_set))
+
+def letters_entropy_analysis(filepath, frame_size, frame_overlap, n, letters_set):
+    results = []
+    for frame in FrameIterator(filepath, frame_size, frame_overlap):
+        results.append(get_letter_entropy(frame, n, letters_set))
+    return results
+
+def word_entropy_analysis(filepath, frame_size, frame_overlap, word_end_set):
+    results = []
+    for frame in FrameIterator(filepath, frame_size, frame_overlap):
+        results.append(get_word_entropy(frame, n, letters_set))
+    return results
